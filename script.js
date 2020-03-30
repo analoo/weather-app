a0672310cc77549158d09312962f439fbdf90249
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-29 17:33:36 -0700

    forecast data rendering function updated to pull from forecast data API call

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function fiveDayForecast() {
         method: "GET"
     }).then(function (response) {
         forecastData = response;
+        renderForecastData();
     })
 }
 
@@ -74,27 +75,26 @@ function renderCurrentData() {
     $("#location").text(city + " " + "(Date Formatted)" + " " + weatherData.weather[0].main);
     let kelvinData = weatherData.main.temp;
     let farenheitData = parseFloat((kelvinData - 273.15) * 9 / 5 + 32).toFixed(1);
-    $("#day-0").append("<p>Temperature: " + farenheitData + "°F</p>")
-    $("#day-0").append("<p>Humidity: " + weatherData.main.humidity + "%</p>")
-    $("#day-0").append("<p>Wind Speed: " + weatherData.wind.speed + " MPH</p>");
-    $("#day-0").append("<p>UV Index: " + ultraViolet + "</p>")
+    $("#current").append("<p>Temperature: " + farenheitData + "°F</p>")
+    $("#current").append("<p>Humidity: " + weatherData.main.humidity + "%</p>")
+    $("#current").append("<p>Wind Speed: " + weatherData.wind.speed + " MPH</p>");
+    $("#current").append("<p>UV Index: " + ultraViolet + "</p>")
 }
-// function renderForecastData() {
-//     for (var i = 0; i < 40; i++) {
-//         var date = weatherData.list[i].dt_txt.split(" ")[0];
-//         var dateFormatted = date;
-//         console.log(weatherData.list[i].dt_txt);
-//         // $("#day-" + i).append("<h3>" + dateFormatted + "</h3>")
-//         // var weatherDesc = weatherData.list[i+8].weather[0].main;
-//         // console.log(weatherDesc);
+function renderForecastData() {
+    var j = 0;
+    for (var i = 0; i < 5; i++) {
+        var date = forecastData.list[j].dt_txt.split(" ")[0];
+        var dateFormatted = date;
+        $("#day-" + i).append("<h4>" + dateFormatted + "</h4>")
+        var weatherDesc = forecastData.list[j].weather[0].main;
+        $("#day-" + i).append("<p>" + weatherDesc + "</p>")
+        var kelvinData = forecastData.list[j].main.temp;
+        var farenheitData = parseFloat((kelvinData - 273.15) * 9 / 5 + 32).toFixed(1);
+        $("#day-" + i).append("<p>Temperature: " + farenheitData + "°F</p>")
+        $("#day-" + i).append("<p>Humidity: " + forecastData.list[j].main.humidity + "%</p>")
+        j+=8
+    }
+}
 
-//         // $("#day-" + i).append("<p>" + weatherDesc + "</p>")
-//         // var kelvinData = weatherData.list[i].main.temp;
-//         // var farenheitData = parseFloat((kelvinData - 273.15) * 9 / 5 + 32).toFixed(1);
-//         // $("#day-" + i).append("<p>Temperature: " + farenheitData + "°F</p>")
-//         // $("#day-" + i).append("<p>Humidity: " + weatherData.list[i].main.humidity + "%</p>")
-//     }
-// }
-// renderWeatherData();
 
 startProgram()
